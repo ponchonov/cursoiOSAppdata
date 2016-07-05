8d873cb1ce41de58784cb24c4c646b8b10cef996
Héctor Cuevas Morfín <poncho@Hectors-MacBook-Pro-2.local>
2016-07-05 11:58:47 -0500

    agenda

---
+++ Agenda/Agenda/Contacto.swift
@@ -0,0 +1,20 @@
+//
+//  Contacto.swift
+//  Agenda
+//
+//  Created by Héctor Cuevas Morfín on 7/5/16.
+//  Copyright © 2016 AppData. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class Contacto: NSObject {
+    
+    dynamic var name = ""
+    dynamic var lng = 0.0
+    dynamic var lat = 0.0
+    dynamic var imageurl = ""
+    dynamic var phoneHouse = ""
+    dynamic var phoneWork = ""
+}
---
+++ Agenda/Pods/Target Support Files/Pods-Agenda/Pods-Agenda-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AgendaVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AgendaVersionString[];
+
